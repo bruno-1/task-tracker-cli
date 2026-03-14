2841b27369b4a58aabbb05121902a3d430228ce7
User <bruno.serafini@pm.me>
2026-03-13 23:46:10 -0300

    refactor: add cli barrel file to centralize command exports
    - create src/cli/index.js
    - re-export addTask, deleteTask, listTasks, markTask and updateTask
    - simplify imports from CLI commands

---
+++ src/cli/index.js
@@ -0,0 +1,5 @@
+export { addTask } from './add-task.js';
+export { deleteTask } from './delete-task.js';
+export { listTasks } from './list-tasks.js';
+export { markTask } from './mark-task.js';
+export { updateTask } from './update-task.js';
